e1e7d0c9bf59c9561899dd039cd9830956b491af
Marco Vavassori <mv@marcovavassori.com>
2023-06-02 15:43:39 +0200

    colors

--- app/passwordreset/page.js
+++ app/passwordreset/page.js
@@ -24,7 +24,7 @@ export default function PasswordReset() {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
-        <h1 className="text-2xl font-semibold text-center text-purple-700">
+        <h1 className="text-2xl font-semibold text-center text-blue-700">
           Reset Password
         </h1>
         <form onSubmit={handlePasswordReset}>
@@ -39,11 +39,11 @@ export default function PasswordReset() {
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               type="email"
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
           <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
               Reset Password
             </button>
           </div>
